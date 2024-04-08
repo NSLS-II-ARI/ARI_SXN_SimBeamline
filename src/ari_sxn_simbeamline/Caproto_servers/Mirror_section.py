27cd32809bbc71ed523afb7dc74f6b487a6c09c9
Andrew Walter <awalter@bnl.gov>
2024-04-08 08:41:36 -0400

    Adding vacuum and baffle PVs to the 'ARI M1' mirror caproto IOC

--- src/ari_sxn_simbeamline/Caproto_servers/Mirror_section.py
+++ src/ari_sxn_simbeamline/Caproto_servers/Mirror_section.py
@@ -3,8 +3,64 @@ This file will contain some generic caproto PVGroups used to generate the beamli
 Caproto IOC servers
 """
 from caproto.ioc_examples.fake_motor_record import FakeMotor
-from caproto.server import PVGroup, SubGroup, ioc_arg_parser, run
+from caproto.server import PVGroup, SubGroup, pvproperty, ioc_arg_parser, run
 from textwrap import dedent
+
+
+class FourBladeCurrent(PVGroup):
+    """
+    A PVGroup that generates the PVs associated with the 4 blade electrometer.
+
+    This class should be used to define the PVs for the 4 blade electrometers used
+    in the ARI and SXN beamlines for the Baffle slits.
+
+    TODO:
+    1. Work out how we want to update the returned current values based on the position
+    of the blades and the upstream mirrors.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)  # call the PVGroup __init__ function
+
+    # Add the current PVs
+    top = pvproperty(value=3E-6, name=':top', read_only=True)
+    bottom = pvproperty(value=3E-6, name=':bottom', read_only=True)
+    inboard = pvproperty(value=3E-6, name=':inboard', read_only=True)
+    outboard = pvproperty(value=3E-6, name=':outboard', read_only=True)
+
+
+class BaffleSlit(PVGroup):
+    """
+    A PVGroup that generates the PVs associated with the ARI M1 mirror system.
+
+    This class should be used to define the Baffle Slit system PVs for the baffle slits
+    used in the ARI and SXN beamlines. It will consist of PVs for each of the associated
+    motors for each baffle as well as the photo-current PVs from each of the blades.
+
+    TODO:
+    1. Work out how we want to define the area detector PVs, including how we 'update'
+       the photo-current PVs based from each of the blades when the mirror and/or baffles
+       are moved.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)  # call the PVGroup __init__ function
+
+    # Add the baffle motor PVs.
+    top = SubGroup(FakeMotor, velocity=0.1, precision=6E-3, acceleration=1.0,
+                   resolution=6E-3, user_limits=(-1, 20), tick_rate_hz=10.,
+                   prefix=':top')
+    bottom = SubGroup(FakeMotor, velocity=0.1, precision=6E-3, acceleration=1.0,
+                      resolution=6E-3, user_limits=(-20, 1), tick_rate_hz=10.,
+                      prefix=':bottom')
+    inboard = SubGroup(FakeMotor, velocity=0.1, precision=6E-3, acceleration=1.0,
+                       resolution=6E-3, user_limits=(-20, 1), tick_rate_hz=10.,
+                       prefix=':inboard')
+    outboard = SubGroup(FakeMotor, velocity=0.1, precision=6E-3, acceleration=1.0,
+                        resolution=6E-3, user_limits=(-1, 20), tick_rate_hz=10.,
+                        prefix=':outboard')
+
+    current = SubGroup(FourBladeCurrent, prefix=':current')
 
 
 class AriM1Mirror(PVGroup):
@@ -21,40 +77,50 @@ class AriM1Mirror(PVGroup):
         - This may help create a cohesive connection between them but also blurs
           the lines between vacuum sections and physical devices.
     2. Add the vacuum component (gauges, pumps, valves, ....).
-        - This may require defining vacuum component PVGroups.
+        - Temporary read only PVs have been created but I need to see what other PVs
+        are associated with this hardware that we may want to simulate.
 
     Parameters
     ----------
     *args : list
-        The arguments passed to the PVGroup parent class
+        The arguments passed to the PVGroup parent class.
     **kwargs : list, optional
-        The Keyword arguments passed to the PVGroup parent class
+        The Keyword arguments passed to the PVGroup parent class.
     """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)  # call the PVGroup __init__ function
 
+    # Add the mirror motor PVs.
     Ry_coarse = SubGroup(FakeMotor, velocity=0.1, precision=6E-5, acceleration=1.0,
                          resolution=6E-5, user_limits=(-3.15, -1.15), tick_rate_hz=10.,
-                         prefix='Ry_coarse')
+                         prefix=':Ry_coarse')
     Ry_fine = SubGroup(FakeMotor, velocity=0.1, precision=6E-7, acceleration=1.0,
                        resolution=6E-7, user_limits=(-0.03, 0.03), tick_rate_hz=10.,
-                       prefix='Ry_fine')
+                       prefix=':Ry_fine')
     Rz = SubGroup(FakeMotor, velocity=0.1, precision=6E-5, acceleration=1.0,
                   resolution=6E-5, user_limits=(-2.3, 2.3), tick_rate_hz=10.,
-                  prefix='Rz')
+                  prefix=':Rz')
     x = SubGroup(FakeMotor, velocity=0.0001, precision=5., acceleration=1.0,
                  resolution=5., user_limits=(-10000., 10000.), tick_rate_hz=1E-3,
-                 prefix='x')
+                 prefix=':x')
     y = SubGroup(FakeMotor, velocity=0.1, precision=5., acceleration=1.0,
                  resolution=5., user_limits=(-10000., 10000.), tick_rate_hz=10.,
-                 prefix='y')
+                 prefix=':y')
+
+    # Add the mirror chamber vacuum PVs.
+    ccg = pvproperty(value=3E-10, name=':ccg', read_only=True)
+    tcg = pvproperty(value=1E-4, name=':tcg', read_only=True)
+    ip = pvproperty(value=4E-10, name=':ip', read_only=True)
+
+    # Add the baffle slit PVs.
+    baffle = SubGroup(BaffleSlit, prefix=':baffle')
 
 
 # Add some code to start a version of the server if this file is 'run'.
 if __name__ == "__main__":
     ioc_options, run_options = ioc_arg_parser(
-        default_prefix="ARI:M1:",
+        default_prefix="ARI_M1",
         desc=dedent(AriM1Mirror.__doc__))
     ioc = AriM1Mirror(**ioc_options)
     run(ioc.pvdb, **run_options)
