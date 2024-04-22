b83fa35593f5938e5fe9960364fb946237c68cd3
Andrew Walter <awalter@bnl.gov>
2024-04-22 09:14:34 -0400

    Adding the docstring and the __initi__ method to the MotorRecord PVGroup

--- src/ari_sxn_simbeamline/caproto_servers/motor_record.py
+++ src/ari_sxn_simbeamline/caproto_servers/motor_record.py
@@ -13,6 +13,24 @@ class MotorRecord(PVGroup):
     as simulate the 'time' it takes for a motor to move. How this is done
     is described below in the NOTES sections.
 
+    Parameters
+    ----------
+    velocity : float
+        The velocity that the motor should move at in units/s (units is the distance
+        units of the motor axis). This is used in determining how fast the 'move' will
+        happen.
+    acceleration : float
+        The acceleration value that the motor should return when asked in units/s/s
+        (units is the distance units of the motor axis). This is provided as a dummy PV
+        only.
+    range : {'high':float, 'low': float}
+        A dictionary mapping the high and low limits of motion (i.e. the 'range' of
+        motion) to the keys 'high' and 'low'. This is used to trigger the high and
+        low limit indicator PVs if the motor passes these values.
+    min_time_step : float, optional
+        A 'minimum' time step to divide the calculated time into for on-the-fly
+        updating of the self.user_readback attribute (suffix.RBV PV). Default is 0.1.
+
     NOTES:
     1. Unless otherwise listed in the notes below the PVs generated are 'Dummy' PVs
     that are not modified by any inputs, or modify any other PVs, except their own
@@ -34,11 +52,11 @@ class MotorRecord(PVGroup):
                 if self.stop = 1: # if the user requested the motion to stop
                     self.user_setpoint = self.user_readback
                     break
-                elif self.RBV+d_interval > self.limits['high']: # if we trip the high limit
+                elif self.RBV+d_interval > self.motion_range['high']: # if we trip the high limit
                     self.high_limit_switch = 1
                     self.user_setpoint = self.user_readback
                     break
-                elif self.RBV+d_interval < self.limits['low']: # if we trip the low limit
+                elif self.RBV+d_interval < self.motion_range['low']: # if we trip the low limit
                     self.low_limit_switch = 1
                     self.user_setpoint = self.user_readback
                     break
@@ -52,7 +70,14 @@ class MotorRecord(PVGroup):
             self.user_readback = self.user_setpoint # Make the last move and clean up.
             self.moving = 0
             self.done_moving = 1
+
     """
+    def __init__(self, velocity, acceleration, motion_range, *args,
+                 min_time_step=0.1, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.motion_defaults = {'velocity': velocity, 'acceleration': acceleration}
+        self.motion_range = motion_range
+        self.min_time_step = min_time_step
 
 
 # Add some code to start a version of the server if this file is 'run'.
